c6e644654e15638f11f23877c4ce9b3637ab159a
Nadil Diksura <nadildiksure1018@gmail.com>
2026-02-04 14:21:01 +0530

    data grid

--- frontend/src/pages/subpages/payroll/Overview.tsx
+++ frontend/src/pages/subpages/payroll/Overview.tsx
@@ -6,6 +6,52 @@ import TrendingUp from "@mui/icons-material/TrendingUp";
 import { Grid, Typography, Card, CardContent, Box, Stack } from "@mui/material";
 import { PieChart } from "@mui/x-charts/PieChart";
 import Legend from "../../../components/reusable/Legend"; 
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
 
 function Overview() {
   
@@ -97,7 +143,25 @@ function Overview() {
           </Card>
         </Grid>
       </Grid>
-      <Box></Box>
+      <Box mt={3}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          disableColumnMenu
+          disableColumnSorting
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+
+      </Box>
     </>
   );
 }
